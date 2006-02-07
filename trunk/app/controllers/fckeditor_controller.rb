09b9854b074ffe30cd8136a7f8e329ca20755b7f
Michael Moen <michael@underpantsgnome.com>
2006-02-07 05:45:10 +0000

    Incorporate changes from Gregg Kellogg

--- trunk/app/controllers/fckeditor_controller.rb
+++ trunk/app/controllers/fckeditor_controller.rb
@@ -1,152 +1,166 @@
 class FckeditorController < ApplicationController
-	# this may not be very Rubyesque, it's my first run at Ruby code
-	# I'm sure it will get better over time
-	# feel free to point out better ways to do thigs in Ruby
-	# if you can do it without being abusive ;)
+  # this may not be very Rubyesque, it's my first run at Ruby code
+  # I'm sure it will get better over time
+  # feel free to point out better ways to do thigs in Ruby
+  # if you can do it without being abusive ;)
+  before_filter :setup, :except => 'index'
+  
+  def index
+    if RAILS_ENV != 'development'
+      render(:nothing => true)
+    end
+  end
 
-	def index
-		if RAILS_ENV != 'development'
-			render(:nothing => true)
-		end
-	end
+  # figure out who needs to handle this request
+  def connector    
+    if @params[:Command] == 'GetFoldersAndFiles' || @params[:Command] == 'GetFolders'
+      get_folders_and_files(@params)
+    elsif @params[:Command] == 'CreateFolder'
+      create_folder(params)
+    elsif @params[:Command] == 'FileUpload'
+      upload_file(@params)
+    end
+  end
 
-	# figure out who needs to handle this request
-	def connector
-		if params[:Command] == 'GetFoldersAndFiles' || params[:Command] == 'GetFolders'
-			get_folders_and_files(params)
-		elsif params[:Command] == 'CreateFolder'
-			create_folder(params)
-		elsif params[:Command] == 'FileUpload'
-			upload_file(params)
-		end
-	end
+  private
+  def setup
+    defaultParams
+    status = 200
+    
+    # Check parameters for correctness
+    if (@params[:Command] !~ /^(GetFolders|GetFoldersAndFiles|CreateFolder|FileUpload)$/)
+      render :text => "Command parameter malformed: \"#{@params[:Command]}\"", status => 405
+    elsif (@params[:Type] !~ /^(File|Image|Flash|Media)$/)
+      render :text => "Type parameter malformed: \"#{@params[:Type]}\"", status => 405
+    elsif (@params[:CurrentFolder] !~ /^\/(.+\/)*$/)
+      render :text => "CurrentFolder parameter malformed: \"#{@params[:CurrentFolder]}\"", status => 405
+    else
+      # Make sure necessary directories exist
+      unless File.directory?(@params[:ServerPath])
+        Dir.mkdir(@params[:ServerPath])
+      end   
+      @dir = "#{params[:ServerPath]}#{params[:Type]}#{params[:CurrentFolder]}"
+      @url = "/UserFiles/#{params[:Type]}#{params[:CurrentFolder]}"
+      @error = 0
+    end
+  end
 
-	# generate a directory listing
-	def get_folders_and_files(params)
-		params		= appendParams(params)
-		@files		= Array.new
-		@dirs		= Array.new
-		d			= Dir.new(params[:dir])
-		f_count		= 0
-		d_count 	= 0
+  # generate a directory listing
+  def get_folders_and_files(params)
+    @files    = Array.new
+    @dirs     = Array.new
 
-		d.each  { |x|
-			# skip . and .. I'm sure there's a better way to handle this
-			if x != '.' && x != '..'
-				# actual file system path
-				full_path = "#{params[:dir]}/#{x}";
+    d = Dir.new(@dir)
 
-				# found a directory add it to the list
-				if File.directory?(full_path)
-					@dirs[d_count] = x
-					d_count = d_count.next
-				#if we only want directories, skip the files
-				elsif params[:Command] == 'GetFoldersAndFiles' && File.file?(full_path)
-					size = File.size(full_path)
+    d.each  { |x|
+      # skip . and .. I'm sure there's a better way to handle this
+      if x != '.' && x != '..'
+        # actual file system path
+        full_path = "#{@dir}/#{x}";
 
-					if size != 0 && size < 1024
-						size = 1
-					else
-						size = File.size(full_path) / 1024
-					end
+        # found a directory add it to the list
+        if File.directory?(full_path)
+          @dirs << x
+        #if we only want directories, skip the files
+        elsif params[:Command] == 'GetFoldersAndFiles' && File.file?(full_path)
+          size = File.size(full_path)
 
-					@files[f_count] = { :name => x, :size => size };
-					f_count = f_count.next
-				end
-			end
-		}
+          if size != 0 && size < 1024
+            size = 1
+          else
+            size = File.size(full_path) / 1024
+          end
 
-		render(:template => 'fckeditor/files_and_folders', :layout => false)
+          @files << { :name => x, :size => size };
+        end
+      end
+    }
 
-	rescue
-		render(:template => 'fckeditor/files_and_folders', :layout => false)
+    render(:template => 'fckeditor/files_and_folders', :layout => false)
 
-	end
+  rescue
+    render(:template => 'fckeditor/files_and_folders', :layout => false)
 
-	# create a new directory
-	def create_folder(params)
-		params		= appendParams(params)
-		new_dir		= params[:dir] + params[:NewFolderName]
-		d			= Dir.mkdir(new_dir)
+  end
 
-		render(:template => 'fckeditor/create_folder', :layout => false)
+  # create a new directory
+  def create_folder(params)
+    new_dir   = @dir + params[:NewFolderName]
+    d = Dir.mkdir(new_dir)
 
-	rescue Errno::EEXIST
-		# directory already exists
-		@error		= 101
-		render(:template => 'fckeditor/create_folder', :layout => false)
-	rescue Errno::EACCES
-		# permission denied
-		@error		= 103
-		render(:template => 'fckeditor/create_folder', :layout => false)
-	rescue
-		# any other error
-		@error		= 110
-		render(:template => 'fckeditor/create_folder', :layout => false)
+    render(:template => 'fckeditor/create_folder', :layout => false)
 
-	end
+  rescue Errno::EEXIST
+    # directory already exists
+    @error    = 101
+    render(:template => 'fckeditor/create_folder', :layout => false)
+  rescue Errno::EACCES
+    # permission denied
+    @error    = 103
+    render(:template => 'fckeditor/create_folder', :layout => false)
+  rescue
+    # any other error
+    @error    = 110
+    render(:template => 'fckeditor/create_folder', :layout => false)
 
-	# upload a new file
-	# currently the FCKeditor docs only allow for 2 errors here
-	# file renamed and invalid file name
-	# not sure how to catch invalid file name yet
-	# I'm thinking there should be a permission denied error as well
-	def upload_file(params)
-		params		= appendParams(params)
-		counter		= 1
-		@file_name	= params[:NewFile].original_filename
-		path		= nil
-		ext			= nil
+  end
 
-		# break it up into file and extension
-		# we need this to check the types and to build new names if necessary
-		/^(.*)(\..*)$/.match(@file_name)
-		path		= Regexp.last_match(1)
-		ext			= Regexp.last_match(2)
+  # upload a new file
+  # currently the FCKeditor docs only allow for 2 errors here
+  # file renamed and invalid file name
+  # not sure how to catch invalid file name yet
+  # I'm thinking there should be a permission denied error as well
+  def upload_file(params)
+    counter   = 1
+    @file_name  = params[:NewFile].original_filename
 
-		# check to make sure this extension isn't in deny and is in allow
-		if ! in_array(FCKEDITOR_FILE_TYPES[params[:Type]]['deny'], ext) &&
-			 in_array(FCKEDITOR_FILE_TYPES[params[:Type]]['allow'], ext)
+    # break it up into file and extension
+    # we need this to check the types and to build new names if necessary
+    ext = File.extname(@file_name)
+    path = File.basename(@file_name, ext)
 
-			while File.exist?("#{params[:dir]}#{@file_name}")
-				@file_name		= "#{path}(#{counter})#{ext}"
-				params[:error]	= 201
-				counter			= counter.next
-			end
+    # check to make sure this extension isn't in deny and is in allow
+    if ! in_array(FCKEDITOR_FILE_TYPES[params[:Type]]['deny'], ext)
+       in_array(FCKEDITOR_FILE_TYPES[params[:Type]]['allow'], ext)
+      while File.exist?("#{@dir}#{@file_name}")
+        @file_name    = "#{path}(#{counter})#{ext}"
+        @error  = 201
+        counter     = counter.next
+      end
 
-			File.open("#{params[:dir]}#{@file_name}", 'w') { |f| f.write(params[:NewFile].read) }
-		else
-			# invalid file type
-			params[:error]	= 202
-		end
+      n = File.open("#{@dir}#{@file_name}", 'wb') { |f| f.write(params[:NewFile].read) }
+    else
+      # invalid file type
+      @error  = 202
+   end
 
-		render(:template => 'fckeditor/upload_file', :layout => false)
-	end
+    render(:template => 'fckeditor/upload_file', :layout => false)
+  end
 
-	# helper to setup pathing info that is common to all methods
-	# is there is a more Rubyesque way to do this?
-	# somebody let me know
-	def appendParams(params)
-		params[:error]		= 0
-		params[:base_dir]	= FCKEDITOR_UPLOADS || 'UserFiles'
-		params[:url]		= "#{params[:base_dir]}#{params[:Type]}#{params[:CurrentFolder]}".gsub(/\/+/, '/')
-		params[:dir]		= "#{RAILS_ROOT}/public#{params[:url]}"
+  # helper to setup pathing info that is common to all methods
+  def defaultParams
+    if RAILS_ENV == 'production' or !@params.has_key?(:ServerPath)
+      # Allow destination directory to be overridden, otherwise it must be that which is configured
+      @params[:ServerPath]  = "#{RAILS_ROOT}/public/UserFiles/"
+    end
+    
+    # Set defaults
+    @params[:Type]            = "Image" unless @params.has_key?(:Type)
+    @params[:CurrentFolder]   = "/" unless @params.has_key?(:CurrentFolder)
+  end
 
-		return params
-	end
+  # helper to see if a value exists in an array
+  # I'm sure there is a more Rubyesque way to do this
+  # somebody let me know
+  def in_array(haystack, needle)
+    if haystack != nil && needle != nil
+      haystack.each { |val|
+        if val == needle
+          return true
+        end
+      }
+    end
 
-	# helper to see if a value exists in an array
-	# I'm sure there is a more Rubyesque way to do this
-	# somebody let me know
-	def in_array(haystack, needle)
-		if haystack != nil && needle != nil
-			haystack.each { |val|
-				if val == needle
-					return true
-				end
-			}
-		end
-
-		return false
-	end
+    return false
+  end
 end
--- trunk/test/functional/fckeditor_controller_test.rb
+++ trunk/test/functional/fckeditor_controller_test.rb
@@ -1,18 +1,194 @@
 require File.dirname(__FILE__) + '/../test_helper'
 require 'fckeditor_controller'
+require 'fileutils'
+include FileUtils
 
 # Re-raise errors caught by the controller.
 class FckeditorController; def rescue_action(e) raise e end; end
 
 class FckeditorControllerTest < Test::Unit::TestCase
+  TMP_DIR = "tmp/"
+  IMG_DIR = "Image"
+  IMG_SRC = "../../public/images"
+  IMG1 = "rails.png"
+  IMG2 = "rails(1).png"
+  
   def setup
     @controller = FckeditorController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+    
+    @test_dir = File.dirname($PROGRAM_NAME)
+    @tmp_dir = "#{@test_dir}/#{TMP_DIR}"
+    @img_dir = "#{@tmp_dir}/#{IMG_DIR}"
+    
+    # Create test upload structure
+    rm_rf(@tmp_dir)
+    Dir.mkdir(@tmp_dir)
+    Dir.mkdir(@img_dir)
+    Dir.mkdir("#{@img_dir}/foo")
+    
+    # Copy a test file into the structure
+    cp("#{@test_dir}/#{IMG_SRC}/#{IMG1}", @img_dir)
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def teardown
+    rm_rf(@tmp_dir)
+  end
+   
+  # Get directory and file list
+  def test_get_folders_and_files
+    get :connector, { :Command => 'GetFoldersAndFiles', :ServerPath => @tmp_dir}
+    
+    assert_response :success
+    assert_template 'files_and_folders'
+    assert_tag :tag => 'connector',
+               :attributes => {:command => "GetFoldersAndFiles", :resourcetype => 'Image'}
+    assert_tag :tag => 'currentfolder',
+               :attributes => {:url => "/UserFiles/Image/", :path => "/"},
+               :parent => {:tag => "connector"}
+    assert_tag :tag => 'folders',
+               :parent => {:tag => "connector"}
+    assert_tag :tag => 'folder',
+               :attributes => {:name => "foo"},
+               :parent => {:tag => "folders"}
+    assert_tag :tag => 'files',
+               :parent => {:tag => "connector"}
+    assert_tag :tag => 'file',
+               :parent => {:tag => "files"},
+               :attributes => {:name => IMG1}
+  end
+   
+  # Get directory and file list
+  def test_get_folders
+    get :connector, { :Command => 'GetFolders', :ServerPath => @tmp_dir}
+    
+    assert_response :success
+    assert_template 'files_and_folders'
+    assert_tag :tag => 'connector',
+               :attributes => {:command => "GetFolders", :resourcetype => 'Image'}
+    assert_tag :tag => 'currentfolder',
+               :attributes => {:url => "/UserFiles/Image/", :path => "/"},
+               :parent => {:tag => "connector"}
+    assert_tag :tag => 'folders',
+               :parent => {:tag => "connector"}
+    assert_tag :tag => 'folder',
+               :attributes => {:name => "foo"},
+               :parent => {:tag => "folders"}
+    assert_no_tag :tag => 'file'
+  end
+
+  # File Upload
+  def test_file_upload
+    # Make sure there are no files uploaded
+    get :connector, { :Command => 'GetFoldersAndFiles', :ServerPath => @tmp_dir, :CurrentFolder => '/foo/'}
+    assert_response :success
+    assert_template 'files_and_folders'
+    assert_no_tag :tag => 'file',
+                  :attributes => {:name => IMG1}
+
+    # Do the upload
+    heman = uploaded_jpeg("#{@img_dir}/#{IMG1}")
+    post :connector, { :Command => 'FileUpload', :ServerPath => @tmp_dir, :CurrentFolder => '/foo/', :NewFile => heman}
+    assert_response :success
+    assert_template 'upload_file'
+    assert_tag :tag => 'script',
+               :attributes => {:type => "text/javascript"},
+               :content => "UploadCompleted(0, '#{IMG1}'"
+
+    # Make sure the file exists
+    get :connector, { :Command => 'GetFoldersAndFiles', :ServerPath => @tmp_dir, :CurrentFolder => '/foo/'}
+    assert_response :success
+    assert_template 'files_and_folders'
+    assert_tag :tag => 'file',
+               :attributes => {:name => IMG1}
+  end
+
+  # File Upload (muliple)
+  def test_file_upload_multiple
+    # Do the upload
+    heman = uploaded_jpeg("#{@img_dir}/#{IMG1}")
+    post :connector, { :Command => 'FileUpload', :ServerPath => @tmp_dir, :CurrentFolder => '/foo/', :NewFile => heman}
+    assert_response :success
+    assert_template 'upload_file'
+    assert_tag :tag => 'script',
+               :attributes => {:type => "text/javascript"},
+               :content => /UploadCompleted\(0, '#{IMG1}'/
+
+    # Do the upload again
+    post :connector, { :Command => 'FileUpload', :ServerPath => @tmp_dir, :CurrentFolder => '/foo/', :NewFile => heman}
+    assert_response :success
+    assert_template 'upload_file'
+    assert_tag :tag => 'script',
+               :attributes => {:type => "text/javascript"},
+               :content => "UploadCompleted\(201, '#{IMG2}'"  # 201 status return indicates it was renamed
+
+    # Make sure the files exists
+    get :connector, { :Command => 'GetFoldersAndFiles', :ServerPath => @tmp_dir, :CurrentFolder => '/foo/'}
+    assert_response :success
+    assert_template 'files_and_folders'
+    assert_tag :tag => 'file',
+               :attributes => {:name => IMG1}
+    assert_tag :tag => 'file',
+               :attributes => {:name => IMG2}
+  end
+
+  # Get create folder
+  def test_create_folder
+    get :connector, { :Command => 'CreateFolder', :ServerPath => @tmp_dir, :NewFolderName => 'bar'}
+    
+    assert_response :success
+    assert_template 'create_folder'
+    assert_tag :tag => 'connector',
+               :attributes => {:command => "CreateFolder", :resourcetype => 'Image'}
+    assert_tag :tag => 'currentfolder',
+               :attributes => {:url => "/UserFiles/Image/", :path => "/"},
+               :parent => {:tag => "connector"}
+    assert_tag :tag => 'error',
+               :parent => {:tag => "connector"},
+               :attributes => {:number => "0"}
+  end
+
+  # Get create folder
+  def test_create_folder_existing
+    get :connector, { :Command => 'CreateFolder', :ServerPath => @tmp_dir, :NewFolderName => 'foo'}
+    
+    assert_response :success
+    assert_template 'create_folder'
+    assert_tag :tag => 'connector',
+               :attributes => {:command => "CreateFolder", :resourcetype => 'Image'}
+    assert_tag :tag => 'currentfolder',
+               :attributes => {:url => "/UserFiles/Image/", :path => "/"},
+               :parent => {:tag => "connector"}
+    assert_tag :tag => 'error',
+               :parent => {:tag => "connector"},
+               :attributes => {:number => "101"}
+  end
+  # get us an object that represents an uploaded file
+  def uploaded_file(path, content_type="application/octet-stream", filename=nil)
+    filename ||= File.basename(path)
+    t = Tempfile.new(filename)
+    FileUtils.copy_file(path, t.path)
+    (class << t; self; end;).class_eval do
+      alias local_path path
+      define_method(:original_filename) { filename }
+      define_method(:content_type) { content_type }
+    end
+    return t
+  end
+
+  # a PNG helper
+  def uploaded_png(path, filename=nil)
+    uploaded_file(path, 'image/png', filename)
+  end
+
+  # a JPEG helper
+  def uploaded_jpeg(path, filename=nil)
+    uploaded_file(path, 'image/jpeg', filename)
+  end
+
+  # a GIF helper
+  def uploaded_gif(path, filename=nil)
+    uploaded_file(path, 'image/gif', filename)
   end
 end
